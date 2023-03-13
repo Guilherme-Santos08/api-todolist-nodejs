9a506e249552816f4cd41559c7cce0bd0118ebf3
Guiherme Ribeiro <augusto.gui.2208@gmail.com>
2023-03-13 19:22:18 -0300

    feat: create DB with knex + sqlite3

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hellow', () => {
-  return 'Hello World'
+app.get('/hellow', async () => {
+  /*
+  const taskUnique = await knex('tasks')
+    .where('id', '2c330bd5-4263-463a-a8a0-578f16cbaf58')
+    .select('*')
+  const tasks = await knex('tasks').select('*')
+
+  return tasks
+  */
 })
 
 app
