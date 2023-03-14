483bd0d0319050c1d1d067ae757e2b0d31b7bb9b
Guiherme Ribeiro <augusto.gui.2208@gmail.com>
2023-03-13 21:48:59 -0300

    feat: validate user cookies

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -4,22 +4,32 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 
 import { knex } from '../database'
+import { checkSessionIdexists } from '../middlewares/check-session-id-exists'
 
 export async function tasksRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const tasks = await knex('tasks').select()
+  app.addHook('preHandler', async (request) => {
+    console.log(`[${request.method}] ${request.url}`)
+  })
+
+  app.get('/', { preHandler: [checkSessionIdexists] }, async (request) => {
+    const { sessionId } = request.cookies
+
+    const tasks = await knex('tasks').where('session_id', sessionId).select()
 
     return { tasks }
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', { preHandler: [checkSessionIdexists] }, async (request) => {
     const getTasksParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getTasksParamsSchema.parse(request.params)
+    const { sessionId } = request.cookies
 
-    const task = await knex('tasks').where('id', id).first()
+    const task = await knex('tasks')
+      .where({ session_id: sessionId, id })
+      .first()
 
     return { task }
   })
@@ -32,10 +42,22 @@ export async function tasksRoutes(app: FastifyInstance) {
 
     const { title, description } = createTasksBodySchema.parse(request.body)
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+    }
+
+    reply.setCookie('sessionId', sessionId, {
+      path: '/',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    })
+
     await knex('tasks').insert({
       id: randomUUID(),
       title,
       description,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+
 import { env } from './env'
 import { tasksRoutes } from './routes/tasks'
 
 const app = fastify()
 
+app.register(cookie)
 app.register(tasksRoutes, {
   prefix: 'tasks',
 })
