8ea1b204af54f8fc57b34b05565d53ec7aece037
Guiherme Ribeiro <augusto.gui.2208@gmail.com>
2023-03-16 17:45:50 -0300

    fix: fastify/cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
-
 import { tasksRoutes } from './routes/tasks'
+
+import cors from '@fastify/cors'
 
 export const app = fastify()
 
-app.register(cookie)
+app.register(cors, {
+  origin: ['http://localhost:5173'], // lista de domínios permitidos
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // lista de métodos permitidos
+})
 
+app.register(cookie)
 app.register(tasksRoutes, {
   prefix: 'tasks',
 })
